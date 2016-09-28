7e4efc6ed40e97e4a30b22a4b3621f9a30e40255
CapitanLiteral <anaraguen@gmail.com>
2016-09-28 11:57:15 +0200

    Doing ModuleFBXImporter

--- Engine/ModuleFBXImporter.cpp
+++ Engine/ModuleFBXImporter.cpp
@@ -1,3 +1,5 @@
+#include "SDL\include\SDL.h"
+
 #include "ModuleFBXImporter.h"
 #include "Assimp\Assimp\include\scene.h"
 #include "Assimp\Assimp\include\cfileio.h"
@@ -11,7 +13,46 @@ ModuleFBXImporter::ModuleFBXImporter(Application* app, bool start_enabled) : Mod
 {
 }
 
+bool ModuleFBXImporter::Init()
+{
+	struct aiLogStream stream;
+	stream = aiGetPredefinedLogStream(aiDefaultLogStream_DEBUGGER, nullptr);
+	aiAttachLogStream(&stream);
+}
+
+bool ModuleFBXImporter::CleanUp()
+{
+	aiDetachAllLogStreams();
+}
 
 ModuleFBXImporter::~ModuleFBXImporter()
 {
+}
+
+
+void ModuleFBXImporter::loadFBX(char* full_path)
+{
+	const aiScene* scene = aiImportFile(full_path, aiProcessPreset_TargetRealtime_MaxQuality);
+
+	if (scene != nullptr && scene->HasMeshes())
+	{
+		// use scene->mNumMeshes to iterate on scene->mMeshes array
+		meshes = new VramVertex[scene->mNumMeshes];
+
+		for (int i = 0; i < scene->mNumMeshes; i++)
+		{
+			//Todo
+			//VramVertex* mesh = new VramVertex();
+			//aiMesh* new_mesh = scene->mMeshes[i];		
+
+			//meshes->numVertices = new_mesh->mNumVertices;
+			//meshes->vertices = new float[meshes->numVertices * 3];
+			//memcpy(meshes->vertices, new_mesh->mVertices, sizeof(float)*  meshes->numVertices * 3);
+			//SDL_Log("New mesh with %d vertices", meshes->numVertices);
+			
+		}
+		aiReleaseImport(scene);
+	}
+	else
+		SDL_Log("Error loading Scene %s", full_path);
 }
--- Engine/ModuleFBXImporter.h
+++ Engine/ModuleFBXImporter.h
@@ -1,10 +1,32 @@
 #pragma once
 #include "Module.h"
+
+struct VramVertex
+{
+	uint32_t idVertices = 0;
+	uint32_t numVertices = 0;
+	float* vertices = nullptr;
+
+	uint32_t idIndices = 0;
+	uint32_t numIndices = 0;
+	uint32_t* indices = nullptr;
+};
+
+
+
 class ModuleFBXImporter :
 	public Module
 {
+public: 
+	//VramVertex meshes;
 public:
 	ModuleFBXImporter(Application* app, bool start_enabled = true);
+	bool Init();
+	bool CleanUp();
+
 	virtual ~ModuleFBXImporter();
+
+	void loadFBX(char* full_path);
+	VramVertex* meshes = NULL;
 };
 
