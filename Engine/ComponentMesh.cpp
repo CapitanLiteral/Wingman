a6f1c92430be8186ede3b1bcd2cef93bbfc2a3dc
CapitanLiteral <anaraguen@gmail.com>
2016-10-16 15:15:38 +0200

    Mesh update and draw added

--- Engine/ComponentMesh.cpp
+++ Engine/ComponentMesh.cpp
@@ -52,4 +52,45 @@ ComponentMesh::~ComponentMesh()
 	}
 
 }
+
+void ComponentMesh::Update()
+{
+	if (enabled)
+	{
+		draw();
+	}
+}
+
+void ComponentMesh::draw()
+{
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	//vertices
+	glBindBuffer(GL_ARRAY_BUFFER, idVertices);
+	glVertexPointer(3, GL_FLOAT, 0, NULL);
+
+	//normals
+	glBindBuffer(GL_ARRAY_BUFFER, idNormals);
+	glNormalPointer(GL_FLOAT, 0, NULL);
+
+	//indices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idIndices);
+
+	//UV
+	glBindBuffer(GL_ARRAY_BUFFER, idUV);
+	if (idUV != 0)
+	{
+		glTexCoordPointer(3, GL_FLOAT, 0, NULL);
+	}
+
+	glColor4f(0.7f, 0.7f, 0.7f, 1.f); //I should get fbx color TODO
+
+	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, NULL);
+
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
 }
