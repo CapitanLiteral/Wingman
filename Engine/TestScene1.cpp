47a664c97ef5495449f1ac00b614182399634443
CapitanLiteral <anaraguen@gmail.com>
2016-10-16 07:52:03 +0200

    Town Added

--- Engine/TestScene1.cpp
+++ Engine/TestScene1.cpp
@@ -26,10 +26,12 @@ bool TestScene1::Init()
 bool TestScene1::Start()
 { 
 	//This is absolute route... We should change it // ERROR!!
-	App->fbxImporter->loadFBX("../DLL/MechaT.FBX");
+	//App->fbxImporter->loadFBX("../DLL/MechaT.FBX");
+	//App->fbxImporter->loadFBX("../DLL/Brute.fbx");
+	App->fbxImporter->loadFBX("../DLL/Town/Street environment_V01.fbx");
 	//App->fbxImporter->loadFBX("../DLL/Warrior.FBX");
 	//id = App->fbxImporter->loadTextures();
-	id = App->texture->loadTexture("../../lena.png");
+	//id = App->texture->loadTexture("../../lena.png");
 	return true;
 }
 update_status TestScene1::PreUpdate(float dt)
@@ -64,106 +66,107 @@ update_status TestScene1::Update(float dt)
 	////Draw a quad in direct mode
 	
 	glEnable(GL_TEXTURE_2D);
-	id = App->texture->loadTexture("../DLL/Lenna.png");
-	glBindTexture(GL_TEXTURE_2D, id);
-	
-	glBegin(GL_TRIANGLES);
+	//id = App->texture->loadTexture("../DLL/Lenna.png");
+	//glBindTexture(GL_TEXTURE_2D, id);
+	//
+	//glBegin(GL_TRIANGLES);
 
-	float s = 1.f * 2;
+	//float s = 1.f * 2;
 
-	//----------------------- Front
-	
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(s, s, s);	
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(-s, -s, s);	
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, -s, s);
+	////----------------------- Front
+	//
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(s, s, s);	
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(-s, -s, s);	
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, -s, s);
 
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, s, s);	
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(-s, -s, s);	
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(s, s, s);
-	//----------------------- Top
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, s, s);	
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(-s, -s, s);	
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(s, s, s);
+	////----------------------- Top
 
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, s, s);
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(s, s, s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, s, -s);
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, s, s);
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(s, s, s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, s, -s);
 
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, s, s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, s, -s);
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(-s, s, -s);
-	//----------------------- Right
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, s, s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, s, -s);
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(-s, s, -s);
+	////----------------------- Right
 
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(s, s, s);
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(s, -s, s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, s, -s);
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(s, s, s);
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(s, -s, s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, s, -s);
 
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(s, -s, s);
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(s, -s, -s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, s, -s);
-	//----------------------- Left
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(s, -s, s);
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(s, -s, -s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, s, -s);
+	////----------------------- Left
 
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(-s, s, -s);
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, -s, s);
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(-s, s, s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(-s, s, -s);
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, -s, s);
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(-s, s, s);
 
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(-s, s, -s);
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(-s, -s, -s);
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, -s, s);
-	//----------------------- Bottom
-	
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, -s, s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, -s, -s);
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(s, -s, s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(-s, s, -s);
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(-s, -s, -s);
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, -s, s);
+	////----------------------- Bottom
+	//
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, -s, s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, -s, -s);
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(s, -s, s);
 
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, -s, s);
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(-s, -s, -s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, -s, -s);
-	//----------------------- Behind
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, -s, s);
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(-s, -s, -s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, -s, -s);
+	////----------------------- Behind
 
-	glTexCoord2f(1.f, 1.f);
-	glVertex3d(s, s, -s);
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, -s, -s);
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, s, -s);
+	//glTexCoord2f(1.f, 1.f);
+	//glVertex3d(s, s, -s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, -s, -s);
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, s, -s);
 
-	glTexCoord2f(1.f, 0.f);
-	glVertex3d(s, -s, -s);
-	glTexCoord2f(0.f, 0.f);
-	glVertex3d(-s, -s, -s);
-	glTexCoord2f(0.f, 1.f);
-	glVertex3d(-s, s, -s);
+	//glTexCoord2f(1.f, 0.f);
+	//glVertex3d(s, -s, -s);
+	//glTexCoord2f(0.f, 0.f);
+	//glVertex3d(-s, -s, -s);
+	//glTexCoord2f(0.f, 1.f);
+	//glVertex3d(-s, s, -s);
 
-	glEnd();
-	glBindTexture(GL_TEXTURE_2D, 0);
+	//glEnd();
+	//glBindTexture(GL_TEXTURE_2D, 0);
+	//
 	id = App->texture->loadTexture("../DLL/Diffuse_Mech.png");
 	glBindTexture(GL_TEXTURE_2D, id);
 	App->fbxImporter->drawMeshes(App->fbxImporter->meshes);
