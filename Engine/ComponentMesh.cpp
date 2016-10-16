f44b831f463e677a3596fc58a85dc8f4efadb80a
CapitanLiteral <anaraguen@gmail.com>
2016-10-16 15:15:05 +0200

    Component mesh delete added

--- Engine/ComponentMesh.cpp
+++ Engine/ComponentMesh.cpp
@@ -11,4 +11,45 @@ ComponentMesh::ComponentMesh()
 
 ComponentMesh::~ComponentMesh()
 {
+	//Delete arrays
+	if (vertices != nullptr)
+	{
+		delete vertices;
+	}
+	if (indices != nullptr)
+	{
+		delete indices;
+	}
+	if (normals != nullptr)
+	{
+		delete normals;
+	}
+	if (UV != nullptr)
+	{
+		delete UV;
+	}
+
+	// Delete Buffers and restore de ID's
+	if (idVertices != 0)
+	{
+		glDeleteBuffers(1, &idVertices);
+		idVertices = 0;
+	}
+	if (idIndices != 0)
+	{
+		glDeleteBuffers(1, &idIndices);
+		idIndices = 0;
+	}
+	if (idNormals != 0)
+	{
+		glDeleteBuffers(1, &idNormals);
+		idNormals = 0;
+	}
+	if (idUV != 0)
+	{
+		glDeleteBuffers(1, &idUV);
+		idUV = 0;
+	}
+
+}
 }
