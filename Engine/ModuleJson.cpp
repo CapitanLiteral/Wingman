e3a87980bc7d2f0da19c9cf4c59f447d51b87d80
CapitanLiteral <anaraguen@gmail.com>
2016-11-07 09:00:18 +0100

    Cajun integration

--- Engine/ModuleJson.cpp
+++ Engine/ModuleJson.cpp
@@ -3,7 +3,9 @@
 
 #include "ModuleJson.h"
 
-
+#include "cajun\elements.h"
+#include "cajun\reader.h"
+#include "cajun\writer.h"
 
 ModuleJson::ModuleJson(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
